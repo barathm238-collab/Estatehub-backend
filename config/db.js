6b5fe3c90eea636bb51ad98a5e35965509f49009
barath <barathm238@gmail.com>
2025-12-19 11:20:51 +0530

    Db changed to atlas

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/estatehub");
+    await mongoose.connect("mongodb+srv://barath:barath123@cluster0.gtp2qla.mongodb.net/estatehub");
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed", error);
